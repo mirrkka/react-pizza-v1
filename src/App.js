cd1e45de24808b61f0485211749742992ab12041
a.sokolova <a.sokolova@linksat.ru>
2022-08-22 23:32:05 +0300

    get pizzas from json file

--- src/App.js
+++ src/App.js
@@ -6,15 +6,31 @@ import {
   Route,
 } from "react-router-dom";
 import Cart from './pages/Cart';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+    .then((res) => res.json())
+    .then((json) => {
+      setPizzas(json.pizzas)
+    })
+
+  }, [])
+
+  console.log(pizzas)
+
+
   return (
     <BrowserRouter>
       <div className="wrapper">
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home items={pizzas}/>} exact/>
             <Route path="/cart" element={<Cart />} />
 
           </Routes>
